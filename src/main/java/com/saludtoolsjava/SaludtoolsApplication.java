e32cd2e4425291fad61fb3c3b2bc454953d01786
Juan Pablo <jpablo859@hotmail.com>
2022-03-12 06:00:42 -0300

    Configuración inicial

---
+++ src/main/java/com/saludtoolsjava/SaludtoolsApplication.java
@@ -0,0 +1,13 @@
+package com.saludtoolsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludtoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludtoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saludtoolsjava/SaludtoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saludtoolsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludtoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
